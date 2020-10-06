b8cbd837cf4d17130252107d3a90a827d2892685
Samridhi Gupta <68322193+Samridhi264@users.noreply.github.com>
2020-10-06 21:37:47 +0530

    Create Square.java

---
+++ Patterns/Square.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		 int i,j;
+         Scanner s=new Scanner(System.in);  
+         int n=s.nextInt();
+         for(i=1;i<=n;i++)
+         {
+         for(j=1;j<=n;j++)
+         {
+         System.out.print(n);
+         }
+         System.out.println();    
+         }
+		
+	}
+
+}
