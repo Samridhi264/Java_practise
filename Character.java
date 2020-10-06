f522b708c764bd7331eae9287ef8fb9dcb162812
Samridhi Gupta <68322193+Samridhi264@users.noreply.github.com>
2020-10-06 21:41:30 +0530

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		    int i,j,k=65;
+            char chr;
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for(i=1;i<=n;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                chr=(char)(k+j-1);
+                
+                System.out.print(chr);
+            }
+            System.out.println();
+            k=65+i;
+        }
+	
+		
+	}
+
+}
