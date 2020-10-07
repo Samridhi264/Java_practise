cfc4bc132cc98f793fdc793012bb7c7273c3c338
Samridhi Gupta <68322193+Samridhi264@users.noreply.github.com>
2020-10-07 21:53:18 +0530

    Create InvertedNumber.java

---
+++ Patterns/InvertedNumber.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Solution {
+
+
+	public static void main(String[] args) {
+		
+        // Write your code here
+        	
+	int i,j=0,k;
+      Scanner s=new Scanner(System.in);
+      int n=s.nextInt();
+      for(i=n;i>0;i--)
+      {
+         for(k=i;k>0;k--)
+         {
+             System.out.print(i);
+         }
+         System.out.println();
+        
+      }
+
+
+		
+	}
+
+}
