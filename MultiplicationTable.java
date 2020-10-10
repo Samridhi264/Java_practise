23c65cae6ba20078800cd0d01918bf1b171b4de4
Samridhi Gupta <68322193+Samridhi264@users.noreply.github.com>
2020-10-10 23:28:11 +0530

    Create MultiplicationTable.java

---
+++ MultiplicationTable.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main {
+	
+	public static void main(String[] args) {
+        int i;
+	Scanner s=new Scanner(System.in);
+        int num=s.nextInt();
+        for(i=1;i<=10;i++)
+        {
+            System.out.println(num*i);
+        }
+
+	}
+}
