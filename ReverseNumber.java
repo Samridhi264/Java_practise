92cb56860a11904368d7aabe685273999b4c8d70
Samridhi Gupta <68322193+Samridhi264@users.noreply.github.com>
2020-10-11 22:32:55 +0530

    Create ReverseNumber.java

---
+++ ReverseNumber.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner s=new Scanner(System.in);
+        int num=s.nextInt();
+        int rem,sum=0;
+        while(num>0)
+        {
+            rem=num%10;
+            num=num/10;
+            sum=sum*10+rem;
+        }
+System.out.print(sum);
+	}
+}
