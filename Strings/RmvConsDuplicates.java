08379e22911c4644ee699bace8b6a03f6e358fdb
Samridhi Gupta <68322193+Samridhi264@users.noreply.github.com>
2020-10-22 23:36:25 +0530

    Create RmvConsDuplicates.java

---
+++ Strings/RmvConsDuplicates.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class solution {
+
+	public static String removeConsecutiveDuplicates(String str) {
+	    
+   
+        int n =str.length();
+  
+        int res_ind = 1, ip_ind = 1; 
+        char arr[] = str.toCharArray(); 
+          
+        while (ip_ind != arr.length) 
+        { 
+            if(arr[ip_ind] != arr[ip_ind-1]) 
+            { 
+                arr[res_ind] = arr[ip_ind]; 
+                res_ind++; 
+            } 
+            ip_ind++; 
+            
+        } 
+      
+        str = new String(arr); 
+        return str.substring(0,res_ind); 
+    } 
+       
+  
+    static String removeDups(String str) 
+    { 
+      
+       char temp[] = str.toCharArray(); 
+       Arrays.sort(temp); 
+       str = new String(temp); 
+         
+    
+       return removeConsecutiveDuplicates(str); 
+    } 
+	}
+
