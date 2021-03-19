70314fb51287b35543a800bb77f0fa91e177f69a
Samridhi Gupta <68322193+Samridhi264@users.noreply.github.com>
2021-03-19 11:04:05 +0530

    Update CheckPermutation.java

--- Strings/CheckPermutation.java
+++ Strings/CheckPermutation.java
@@ -1,22 +1,20 @@
 import java.util.*;
-public class solution {
+class Solution {
 
 	public static boolean isPermutation(String str1, String str2) {
-
-    int n1 = str1.length();  
-    int n2 = str2.length();  
-    if (n1 != n2)  
-    return false;  
-    char ch1[] = str1.toCharArray(); 
-    char ch2[] = str2.toCharArray(); 
-   
-    Arrays.sort(ch1);  
-    Arrays.sort(ch2);  
-    
-    for (int i = 0; i < n1; i++)  
-    if (ch1[i] != ch2[i])  
-        return false;  
-  
-    return true;
+if(str1.length()!=str2.length())
+    return false;
+        char[] s1=str1.toCharArray();
+        char[] s2=str2.toCharArray();
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+        for(int i=0;i<s1.length;i++)
+        {
+            if(s1[i]!=s2[i])
+            {
+                return false;
+            }
+        }
+        return true;
 	}
 }
